3c882888500933b6771eee20f86f25ab0694ca02
elexfreeman <elextraza@gmail.com>
2024-10-18 23:18:08 +0300

    add base ctrl

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,38 @@
-use actix_web::{get, App, Error, HttpResponse, HttpServer, Result};
+use actix_web::{web, App, HttpServer};
 mod modules;
-use modules::sample::sample_ctrl::sample_route;
-use crate::error_s::response_error;
+use modules::sample::sample_ctrl::*;
 
 mod interfaces;
-use crate::interfaces::product_i::*;
 
 mod system;
 use crate::system::*;
 
-async fn get_data() -> Result<ProductPageI, Error> {
-    // URL, на который будем отправлять запрос
-    let url = "http://lavacacao.ru:1337/api/page-data-common?populate=deep";
-    // Выполняем GET-запрос
-    let data = reqwest::get(url)
-        .await
-        .map_err(actix_web::error::ErrorInternalServerError)?; // <- convert it into an actix_web::Error
-   // Err("error code: 13".to_string()).map_err(actix_web::error::ErrorInternalServerError)?; // <- convert it into an actix_web::Error
-    data.json::<ProductPageI>()
-        .await
-        .map_err(actix_web::error::ErrorInternalServerError) // <- convert it into an actix_web::Error
-}
-
-
-
-#[get("/some_route")]
-async fn some_route() -> Result<HttpResponse, Error> {
-    log::info!("Request from /some_route");
-    fa_action!(get_data(), ProductPageI, response_error).await
-}
+//async fn get_data() -> Result<ProductPageI, Error> {
+//    // URL, на который будем отправлять запрос
+//    let url = "http://lavacacao.ru:1337/api/page-data-common?populate=deep";
+//    // Выполняем GET-запрос
+//    let data = reqwest::get(url)
+//        .await
+//        .map_err(actix_web::error::ErrorInternalServerError)?; // <- convert it into an actix_web::Error
+//                                                               // Err("error code: 13".to_string()).map_err(actix_web::error::ErrorInternalServerError)?; // <- convert it into an actix_web::Error
+//    data.json::<ProductPageI>()
+//        .await
+//        .map_err(actix_web::error::ErrorInternalServerError) // <- convert it into an actix_web::Error
+//}
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     log::info!("starting HTTP server at http://localhost:8080");
-    HttpServer::new(|| {
+    let user_data = web::Data::new(UserData {
+        sample_string: "default_value".to_string(),
+    });
+    HttpServer::new(move || {
         App::new()
-            .service(some_route)
-            .service(sample_route)
+            .app_data(user_data.clone())
+            .service(sample_route_one)
+            .service(sample_route_two)
     })
     .workers(4)
     .bind(("127.0.0.1", 8080))?
--- src/modules/sample/sample_ctrl.rs
+++ src/modules/sample/sample_ctrl.rs
@@ -1,4 +1,4 @@
-use actix_web::{post, web, Error, HttpResponse};
+use actix_web::{post, web, Error, HttpRequest, HttpResponse};
 
 use crate::fa_action;
 use crate::modules::sample::sample_m::get_data;
@@ -6,8 +6,90 @@ use crate::modules::sample::sample_r::Request as SampleRequest;
 use crate::modules::sample::sample_r::Response as SampleResponse;
 use crate::system::error_s::response_error;
 
-#[post("/sample_route")]
-pub async fn sample_route(request: web::Json<SampleRequest>) -> Result<HttpResponse, Error> {
-    log::info!("Request from /some_route");
-    fa_action!(get_data(request), SampleResponse, response_error).await
+use crate::system::BaseCtrl;
+
+
+struct SampleCtrl {
+    base_ctrl: BaseCtrl,
+}
+
+impl SampleCtrl {
+    fn new(req: HttpRequest) -> Self {
+        let base_ctrl = BaseCtrl::new(req);
+        Self { base_ctrl }
+    }
+
+    async fn sample_route_one(
+        &self,
+        request: web::Json<SampleRequest>,
+    ) -> Result<HttpResponse, Error> {
+        let user_data = &self.base_ctrl.get_user_data();
+        log::info!("Request from /some_route");
+        log::info!("User data str: {}", user_data.sample_string);
+        fa_action!(get_data(request), SampleResponse, response_error).await
+    }
+    async fn sample_route_two(
+        &self,
+        request: web::Json<SampleRequest>,
+    ) -> Result<HttpResponse, Error> {
+        log::info!("Request from /some_route_two");
+        fa_action!(get_data(request), SampleResponse, response_error).await
+    }
+}
+
+//
+//
+//
+//trait SampleCtrlTrait {
+//    async fn sample_route_one(
+//        &self,
+//        request: web::Json<SampleRequest>,
+//    ) -> Result<HttpResponse, Error>;
+//
+//    async fn sample_route_two(
+//        &self,
+//        request: web::Json<SampleRequest>,
+//    ) -> Result<HttpResponse, Error>;
+//}
+//
+//
+//impl SampleCtrlTrait for BaseCtrl {
+//    async fn sample_route_one(
+//        &self,
+//        request: web::Json<SampleRequest>,
+//    ) -> Result<HttpResponse, Error> {
+//        let data = &self.req.app_data::<Data<Mutex<UserData>>>().unwrap();
+//        let mut user_data = data.lock().unwrap();
+//        log::info!("Request from /some_route");
+//        log::info!("User data str: {}", user_data.sample_string);
+//        fa_action!(get_data(request), SampleResponse, response_error).await
+//    }
+//
+//    async fn sample_route_two(
+//        &self,
+//        request: web::Json<SampleRequest>,
+//    ) -> Result<HttpResponse, Error> {
+//        log::info!("Request from /some_route_two");
+//        let data = &self.req.app_data::<Data<Mutex<UserData>>>().unwrap();
+//        let mut user_data = data.lock().unwrap();
+//        user_data.sample_string = String::from("new_value");
+//        fa_action!(get_data(request), SampleResponse, response_error).await
+//    }
+//}
+//
+#[post("/sample_route_one")]
+pub async fn sample_route_one(
+    body: web::Json<SampleRequest>,
+    req: HttpRequest,
+) -> Result<HttpResponse, Error> {
+//    let sample_ctrl = SampleCtrl::new(req);
+    SampleCtrl::new(req).sample_route_one(body).await
+}
+
+#[post("/sample_route_two")]
+pub async fn sample_route_two(
+    body: web::Json<SampleRequest>,
+    req: HttpRequest,
+) -> Result<HttpResponse, Error> {
+    SampleCtrl::new(req).sample_route_two(body).await
 }
